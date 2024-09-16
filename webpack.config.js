669058e97fbd029756cf8e7307ee04c3f1698e66
DarinelGuillen <darinel.escobar@outlook.com>
2024-09-16 17:14:51 -0600

    Initial setup for Webpack including updates to 'webpack.config.js' and 'package.json'. Added necessary dependencies and configured Webpack for JS and CSS bundling. This setup replaces previous building tools for improved performance and maintainability.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    contentScript: './src/js/contentScript.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    }),
+  ],
+};
